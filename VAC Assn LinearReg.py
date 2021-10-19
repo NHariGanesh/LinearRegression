0c9f5973bdd703d5ac956731be12a95d042a9220
NHariGanesh <91584231+NHariGanesh@users.noreply.github.com>
2021-10-19 12:19:12 +0530

    Add files via upload

--- VAC Assn LinearReg.py
+++ VAC Assn LinearReg.py
@@ -53,86 +53,54 @@ X, Y = datasets.load_diabetes(return_X_y=True)
 # In[9]:
 
 
-X.shape, Y.shape
+from sklearn.model_selection import train_test_split
 
 
 # In[10]:
 
 
-import pandas as pd
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
 
 
 # In[11]:
 
 
-BostonHousing = pd.read_csv("BostonHousing.csv")
-BostonHousing
-
-
-# In[13]:
-
-
-Y = BostonHousing.medv
-Y
-
-
-# In[15]:
-
-
-X = BostonHousing.drop(['medv'], axis=1)
-X
-
-
-# In[16]:
-
-
-from sklearn.model_selection import train_test_split
-
-
-# In[18]:
-
-
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-
-
-# In[19]:
-
-
 X_train.shape, Y_train.shape
 
 
-# In[20]:
+# In[12]:
 
 
 X_test.shape, Y_test.shape
 
 
-# In[27]:
+# In[13]:
 
 
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 
 
-# In[28]:
+# In[14]:
 
 
 model = linear_model.LinearRegression()
 
 
-# In[29]:
+# In[15]:
 
 
 model.fit(X_train, Y_train)
 
 
-# In[30]:
+# In[16]:
 
 
 Y_pred = model.predict(X_test)
 
 
-# In[31]:
+# In[17]:
+
 
 
 print('Coefficients:', model.coef_)
@@ -143,75 +111,87 @@ print('Coefficient of determination (R^2): %.2f'
       % r2_score(Y_test, Y_pred))
 
 
-# In[32]:
+# In[20]:
+
+
+print(diabetes.feature_names)
+
+
+# In[21]:
 
 
 r2_score(Y_test, Y_pred)
 
 
-# In[34]:
+# In[22]:
 
 
 r2_score(Y_test, Y_pred).dtype
 
 
-# In[36]:
+# In[23]:
 
 
 '%f' % 0.523810833536016
 
 
-# In[38]:
+# In[24]:
 
 
 '%.3f' % 0.523810833536016
 
 
-# In[40]:
+# In[25]:
 
 
 '%.2f' % 0.523810833536016
 
 
-# In[41]:
+# In[26]:
 
 
 import seaborn as sns
 
 
-# In[43]:
+# In[27]:
 
 
 Y_test
 
 
-# In[45]:
+# In[28]:
 
 
 import numpy as np
 np.array(Y_test)
 
 
-# In[47]:
+# In[29]:
 
 
 Y_pred
 
 
-# In[49]:
+# In[30]:
 
 
 sns.scatterplot(Y_test, Y_pred)
 
 
-# In[51]:
+# In[31]:
 
 
 sns.scatterplot(Y_test, Y_pred, marker="+")
 
 
-# In[52]:
+# In[32]:
 
 
 sns.scatterplot(Y_test, Y_pred, alpha=0.5)
+
+
+# In[ ]:
+
+
+
 
